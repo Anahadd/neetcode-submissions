8f33ca29f5c698755b07d276c5357d22e8b7c6c7
Anahad Dhaliwal <142919181+Anahadd@users.noreply.github.com>
2026-09-19 16:53:30 -0700

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.py
@@ -0,0 +1,30 @@
+'''
+Sliding Window + Hashmap 
+
+Window is valid -> 
+* if there's no duplicate, we can use a set / hashmap for this. 
+* if the char exists within the hashmap, we move L += 1 otherwise 
+* we increase length, then we set length = max(length, curr)
+
+'''
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        L = 0 
+        length = 0 
+        setChar = set()
+
+        for R in range(len(s)):
+
+            while s[R] in setChar: 
+
+                setChar.discard(s[L])
+                L += 1
+
+            if s[R] not in setChar: 
+                setChar.add(s[R])
+
+            length = max(length, R - L + 1)
+
+        return length 
