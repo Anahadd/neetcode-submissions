457c53480ae4d2f1e2b3205c58b195a0c366806d
Anahad Dhaliwal <142919181+Anahadd@users.noreply.github.com>
2026-09-20 15:20:27 -0700

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # we want to optimize |index2 - index1| and min(index2, index1)
+        # this will yield the greatest area
+        # 2 pointer 
+
+        L = 0 
+        R = len(heights) - 1
+        area = 0
+
+        # 0, 1, 2, 3, 4, 5
+
+        while L < R:
+
+            width = R - L 
+            height = min(heights[R], heights[L])
+            area = max(area, width * height)
+
+            if heights[R] < heights[L]:
+                R -= 1 
+            else: 
+                L += 1
+        
+        return area
+
